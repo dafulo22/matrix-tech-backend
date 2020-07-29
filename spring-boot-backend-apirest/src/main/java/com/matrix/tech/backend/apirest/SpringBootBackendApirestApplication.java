20ffe4bf583666bc8156f0502ff38878826bd05c
dfuentesl <dfuentes@mutualser.org>
2020-07-29 04:26:38 -0500

    Agregando la configuración para habilitar Swagger - Para la documentación de los end points del  API-REST

--- spring-boot-backend-apirest/src/main/java/com/matrix/tech/backend/apirest/SpringBootBackendApirestApplication.java
+++ spring-boot-backend-apirest/src/main/java/com/matrix/tech/backend/apirest/SpringBootBackendApirestApplication.java
@@ -2,8 +2,10 @@ package com.matrix.tech.backend.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootBackendApirestApplication {
 
 	public static void main(String[] args) {
